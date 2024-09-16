b6784a32ee3fca7de934afc8242471547b5b4ff6
a1warez <joeclab2@gmail.com>
2024-09-16 15:03:00 +0300

    dz-commit 14

---
+++ src/TRXT.java
@@ -0,0 +1,25 @@
+public class TRXT {
+    public static void main(String[] args) {
+        String text = "1q23we";
+        // index  порядок
+        //   0       1
+        //   1       2
+        //   2       3
+        int  countLetter = 0;
+        int countDigit = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                ++countLetter;
+                System.out.println("letter " + countLetter);
+            } else if (Character.isDigit(text.charAt(i))) {
+                ++countDigit;
+                System.out.println("digit " + countDigit);
+            }
+        }
+
+        System.out.println("Кол во цифр "+ countLetter);
+        System.out.println("Кол во букв "+ countDigit);
+
+
+    }
+}
