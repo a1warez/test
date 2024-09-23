3edcfbf6ed8e0c2360cb04e587189010edb6264a
a1warez <joeclab2@gmail.com>
2024-09-23 19:13:02 +0300

    Dz Строки

--- src/test1/test.java
+++ src/test1/test.java
@@ -7,57 +7,63 @@ public class test {
         task1();
         task2();
         task3();
-
     }
-
     static void task1() {
-    //         Задача 1. Вывод чётных чисел от 1 до n
-    //        Условие: Напишите программу, которая выводит все НЕчётные числа от 1 до заданного числа n.
-    //        Пример: Для n = 10 программа должна вывести: 1 3 5 7 9
-        int n = 20;
-        for (int i = 0; i <= n; i++) {
-            if (i == 0) {
+        Scanner cs = new Scanner(System.in);
+        System.out.print("Введите текст для получить реверса и сравнения с исходными данными: ");
+        String strings = cs.next();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = strings.length()-1; i >= 0; i--){
+            sb.append(strings.charAt(i));
+        }
+        if (sb.equals(sb)){
+            System.out.println("Палигдром ");
+        }else {
+            System.out.println("Не палиндром");
+        }
+        System.out.println(sb);
+    }
+    static void task2(){
+        Scanner cs = new Scanner(System.in);
+        System.out.print("Введите текст на проверку палиндрома: ");
+        String st = cs.next();
+
+        boolean isPol = true;
+
+        for (int i = 0; i < (st.length() / 2); i++){
+            if (st.charAt(i) == st.charAt(((st.length() - 1) - i))){
                 continue;
             }
-            if (i % 2 != 0) {
-                System.out.print(" " + i);
-            }
+            isPol = false;
+            break;
+        }
+        if (isPol){
+            System.out.println("Палиндром");
+        }else {
+            System.out.println("Не палиндром");
         }
     }
 
-    static void task2() {
-        //        Задача 2. Произведение чисел от 1 до n
-        //        Условие: Напишите программу, которая вычисляет произведение всех чисел от 1 до заданного числа n
-        //        Пример: Для n = 4, результат будет 1 * 2 * 3 * 4 = 24.
-        int n = 4;
-        int composition = 0;
-        Integer j;
-        for (int i = 1; i <= n; i++) {
-            j = composition * i;
-            composition += i;
-            System.out.println(j);
-        }
-
-    }
-
-    static void task3() {
-    //        Задача 3. Таблица умножения
-    //        Условие: Напишите программу, которая выводит таблицу умножения от 1 до 10 (1 * 1, 1* 2 и т.д. до 10 * 10)
-    //        Использовать вложенные циклы for.
+    static void task3(){
         Scanner cs = new Scanner(System.in);
-        System.out.print("На что умножим? (1,2,3,4,5,6,7,8,9,10): ");
-        int number1 = cs.nextInt();
+        System.out.print("Введите число на проверку палиндрома: ");
+        Integer num = cs.nextInt();
+        String st = String.valueOf(num);
+        boolean isPol = true;
 
-        for (int i = 1; i < 11; i++) {
-            int num = i * number1;
-            System.out.println(num);
-
-
+        for (int i = 0; i < (st.length() / 2); i++){
+            if (st.charAt(i) == st.charAt(((st.length() - 1) - i))){
+                continue;
+            }
+            isPol = false;
+            break;
         }
-
-
-
-
+        if (isPol){
+            System.out.println("Палиндром");
+        }else {
+            System.out.println("Не палиндром");
+        }
 
 
 
