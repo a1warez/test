79d3f2f7eef4ae35ed9b51ffab55aae6a4bd1cee
a1warez <joeclab2@gmail.com>
2024-09-26 16:27:21 +0300

    Dz Массивы

--- src/dz/Main.java
+++ src/dz/Main.java
@@ -1,59 +1,23 @@
 package dz;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
 
-    }
+        int[] num = {15, 2, 35, 40, 57};
 
-    static void task1() {
-        //         Задача 1. Вывод чётных чисел от 1 до n
-        //        Условие: Напишите программу, которая выводит все НЕчётные числа от 1 до заданного числа n.
-        //        Пример: Для n = 10 программа должна вывести: 1 3 5 7 9
-        int n = 20;
-        for (int i = 0; i <= n; i++) {
-            if (i == 0) {
-                continue;
-            }
-            if (i % 2 != 0) {
-                System.out.print(" " + i);
-            }
-        }
-    }
+        int a = num.length;
+        int b = (a / 2);
+        int s = num[b];
+        System.out.println("Среднее значение: " + s);
 
-    static void task2() {
-        //        Задача 2. Произведение чисел от 1 до n
-        //        Условие: Напишите программу, которая вычисляет произведение всех чисел от 1 до заданного числа n
-        //        Пример: Для n = 4, результат будет 1 * 2 * 3 * 4 = 24.
-        int n = 4;
-        int composition = 0;
-        Integer j;
-        for (int i = 1; i <= n; i++) {
-            j = composition * i;
-            composition += i;
-            System.out.println(j);
-        }
+        double[] nums = {5.2, 13.2, 2.6, 9.8, -3.2};
 
-    }
-
-    static void task3() {
-        //        Задача 3. Таблица умножения
-        //        Условие: Напишите программу, которая выводит таблицу умножения от 1 до 10 (1 * 1, 1* 2 и т.д. до 10 * 10)
-        //        Использовать вложенные циклы for.
-        Scanner cs = new Scanner(System.in);
-        System.out.print("На что умножим? (1,2,3,4,5,6,7,8,9,10): ");
-        int number1 = cs.nextInt();
-
-        for (int i = 1; i < 11; i++) {
-            int num = i * number1;
-            System.out.println(num);
-
-
-        }
+        double max = Arrays.stream(nums).max().getAsDouble();
+        System.out.println("Максимальное значение: " + max);
+        double min = Arrays.stream(nums).min().getAsDouble();
+        System.out.println("Минимальное значение " + min);
 
     }
 }
