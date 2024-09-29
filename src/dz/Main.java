0794b3a4c8123abb65eedc32ff848fea17282cf2
a1warez <joeclab2@gmail.com>
2024-09-29 14:28:22 +0300

    Dz МассивыV2

--- src/dz/Main.java
+++ src/dz/Main.java
@@ -10,7 +10,8 @@ public class Main {
         task4();
         task5();
     }
-    static void task1(){
+
+    static void task1() {
         int[] num = {15, 2, 35, 40, 57};
 
         int sum = 0;
@@ -22,7 +23,8 @@ public class Main {
 
         System.out.println("Среднее арефметическое значение: " + average);
     }
-    static void task2(){
+
+    static void task2() {
         double[] numbers = {5.2, 13.2, 2.6, 9.8, -3.2};
         double min = numbers[0];
         double max = numbers[0];
@@ -37,37 +39,36 @@ public class Main {
 
         }
         System.out.println("Макимальнон значение: " + max);
-        System.out.println("Минемальное значение:" + min);
+        System.out.println("Минимальное значение:" + min);
     }
-    static void task3(){
+
+    static void task3() {
         char[] chars = {'a', 'B', 'i', 'E', 'r', 't', 'o', 'U'};
+
+        char[] vowels = {'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'};
 
         int sum = 0;
         for (int i = 0; i < chars.length; i++) {
-
-            if (chars[i] == 'a' || chars[i] == 'e' || chars[i] == 'i' || chars[i] == 'o' || chars[i] == 'u') {
-                sum++;
-            }
-            if (chars[i] == 'A' || chars[i] == 'E' || chars[i] == 'I' || chars[i] == 'O' || chars[i] == 'U') {
-                sum++;
+            for (int j = 0; j < vowels.length; j++) {
+                if (chars[i] == vowels[j]) {
+                    sum++;
+                }
             }
         }
         System.out.println("Количество гласных букв: " + sum);
 
     }
-    static void task4(){
+
+    static void task4() {
         String str = "Hello World This Is Java";
         char[] charstr = new char[str.length()];
         for (int i = 0; i < str.length(); i++) {
             charstr[i] = str.charAt(i);
-
-        }
-        for (int i = 0; i < charstr.length; i++) {
             if (charstr[i] == ' ') {
                 charstr[i] = '_';
             }
         }
-        System.out.println(Arrays.toString(charstr));
+        System.out.println(charstr);
     }
 
     static void task5() {
